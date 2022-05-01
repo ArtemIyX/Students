70ff9ee798e9ca311b3032463243a2ddbcaad76b
1play2fun123 <1play2fun123@gmail.com>
2022-05-01 18:16:24 +0300

    Group code in Application_Group.cpp

---
+++ Students_app/Application_Group.cpp
@@ -0,0 +1,89 @@
+#include "Application.h"
+#include "Manager.h"
+#include "MenuPosition.h"
+#include "Tools.h"
+
+void UApplication::GroupSection()
+{
+	Menu->ChangeMenuPosition(1); //TODO: Change magic numbers
+}
+
+void UApplication::Group_Add()
+{
+	Menu->Print(ApplicationMessages::Group::MSG_Enter);
+	std::string input;
+	std::cin >> input; // change on getline
+
+	FGroup info(input);
+	UGroup* group = UGroup::CreateGroup(info);
+	Manager->GetGroupManager()->AddInstance(group);
+
+	Menu->Print(String::format("Group %s created\n", group->GetData().Title.c_str()));
+	Menu->Wait();
+}
+
+void UApplication::Group_Show()
+{
+	const std::vector<UGroup*> groups = Manager->GetGroupManager()->GetAllInstances();
+	if (!Group_Check()) {
+		Menu->Print(ApplicationMessages::Group::MSG_No);
+		Menu->Wait();
+		return;
+	}
+	Menu->Print(ApplicationMessages::Group::MSG_Name);
+	for (size_t i = 0; i < groups.size(); ++i)
+	{
+		UGroup* group = groups[i];
+		const FGroup& data = group->GetData();
+		Menu->Print(String::format("[%d]\t%s\n", i + 1, data.Title.c_str()));
+	}
+}
+
+void UApplication::Group_Remove()
+{
+	if (!Group_Check()) {
+		Menu->Print(ApplicationMessages::Group::MSG_No);
+		Menu->Wait();
+		return;
+	}
+	UInstanceManager<UGroup>* groupManager = Manager->GetGroupManager();
+	uint16_t index = 0;
+	bool undo = false;
+	UGroup* group = SelectInstance<UGroup>(groupManager, std::bind(&UApplication::Group_Show, this), index, undo);
+	if (undo) {
+		return;
+	}
+	groupManager->RemoveInstanceAt(index - 1);
+	Menu->Print(ApplicationMessages::Group::MSG_Rem);
+	Menu->Wait();
+}
+
+void UApplication::Group_Edit()
+{
+	const std::vector<UGroup*> groups = Manager->GetGroupManager()->GetAllInstances();
+	const size_t size = groups.size();
+	if (size == 0) {
+		Menu->Print(ApplicationMessages::Group::MSG_No);
+		Menu->Wait();
+		return;
+	}
+	uint16_t index = 0;
+	bool undo = false;
+	UInstanceManager<UGroup>* groupManager = Manager->GetGroupManager();
+	UGroup* group = SelectInstance<UGroup>(groupManager, std::bind(&UApplication::Group_Show, this), index, undo);
+	if (undo) {
+		return;
+	}
+	Menu->Print(ApplicationMessages::Group::MSG_Enter);
+	std::string input;
+	std::cin >> input;
+	FGroup& data = group->GetData();
+	data.Title = input;
+	Menu->Print(ApplicationMessages::Group::MSG_Edit);
+	Menu->Wait();
+}
+
+bool UApplication::Group_Check()
+{
+	return Manager->GetGroupManager()->GetAllInstances().size() != 0;
+}
